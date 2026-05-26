0597aa1e01a962b27fd0e9464205a3fadd42280c
Preeti Yuankrathok <preetisatit@gmail.com>
2026-05-26 17:41:20 +0700

    Prettier format

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss(), cloudflare()],
